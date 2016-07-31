af4cbe27f54b3b27ad94af689ba7526e757bd630
Kylie <kyliepace@gmail.com>
2016-07-30 18:29:20 -0700

    hacked together ok, but seems buggy. need to make back arrow functional, and add drop-down menu type.

--- public/redux/reducers.js
+++ public/redux/reducers.js
@@ -27,19 +27,21 @@ var infoOrderReducer= function(state, action) {
         var question = state[questionSet].questions[counter]; //which question?
         //send infoOrder.questions[counter] to server 
 
-        //increase infoOrder.counter ++
-        var index = 0;
-        for (var i=0; i<question.length; i++){
-            if(question[i].selection){
-                index = question[i].selection.indexOf(question[i].question); //find which of the selection choices was clicked
-            }
-        } //if the question has a drop-down list or a set of possible answer, update the index to reflect whichever was chosen
-        console.log(index);
+       //increase infoOrder.counter ++
         var changeCounterBy = function(){
-            return question[0].changeCounter[index]; //appropriate number to add to counter
+            if(question.length>1){
+                return question[0].changeCounter[0]; //appropriate number to add to counter
+            }
+            else{
+                return question.changeCounter[0]; //appropriate number to add to counter
+            }
+            
         }
+        console.log(question.changeCounter);
         console.log(changeCounterBy());
         counter += changeCounterBy();
+        console.log(counter);
+
         var newCounter = Object.assign({}, state[questionSet], {counter: counter}); //update state with new counter
         // var before = state[questionSet].slice(0, state[questionSet].length - 1);
         // var after = state[questionSet].slice(state[questionSet].length);
@@ -62,6 +64,13 @@ var infoOrderReducer= function(state, action) {
         var answer = action.answer;
         console.log('answer:'+answer);
 
+        //update the question
+        var newQuestion = Object.assign({}, question, {answer: answer});
+        var before = state[questionSet].questions.slice(0, counter);
+        var after = state[questionSet].questions.slice(counter+1);
+        var newQuestions = before.concat(newQuestion, after);
+        console.log(newQuestions);
+
         //increase infoOrder.counter ++
         var changeCounterBy = function(){
             return question.changeCounter[answer]; //appropriate number to add to counter
@@ -70,11 +79,8 @@ var infoOrderReducer= function(state, action) {
         console.log(changeCounterBy());
         counter += changeCounterBy();
         console.log(counter);
-        var newQuestion = Object.assign({}, question, {answer: answer});
-        
-        var before = state[questionSet].questions.slice(0, counter);
-        var after = state[questionSet].questions.slice(counter+1);
-        var newQuestions = before.concat(newQuestion, after);
+
+        //update the state
         var newObject = {questions: newQuestions, counter: counter};
         return Object.assign({}, state, {[questionSet]: newObject});
     }
