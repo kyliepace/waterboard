1709c7a74fa1221f711ed35cd40cfef02d6237ed
Kylie <kyliepace@gmail.com>
2016-08-07 13:26:22 -0700

    trying again

---
+++ webpack-prod.config.js
@@ -0,0 +1,28 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+	
+	entry:[
+		'./index.jsx'
+	],
+	output: {
+		path: __dirname + '/public',
+		filename: 'index_bundle.js'
+	},
+	devServer: {
+    	inline: true,
+    	port: 8080
+  	},
+	module:{
+		loaders:[
+		{test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader", query:{presets: ['react', 'es2016']}}
+		]
+	},
+	plugins: [HTMLWebpackPluginConfig]
+
+}
