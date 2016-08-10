7627860d24f829bd3fcc98d3850b2eed9ec66422
Kylie <kyliepace@gmail.com>
2016-08-10 11:22:02 -0700

    Update server.js
    added waterRights/submit PUT and outlined createPDF code

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var	jsonParser = bodyParser.json();
+var jsonParser = bodyParser.json();
 require('isomorphic-fetch');
-var data = require('./data.js');
-
+var PDFDocument = require 'pdfkit'; //added pdfkit
+var fs = require('fs'); //added fs
+var infoOrderData = require('./data.js');
+var waterRightdata = require('./waterRightdata.js');
 var app = express();
 
 var port =  process.env.PORT || 8080;
@@ -17,15 +19,15 @@ app.post('/logIn', function(req, res){
 	var idCode = req.body.idCode;
 	var password = req.body.password;
 	console.log(idCode);
-	for(var i=0; i<data.length; i++){
-		if(data[i].idCode === idCode && data[i].password === password){
+	for(var i=0; i<infoOrderData.length; i++){
+		if(infoOrderData[i].idCode === idCode && infoOrderData[i].password === password){
 			var savedData = {
-				owner: data[i].owner, 
-				address: data[i].address, 
-				answers: data[i].answers,
-				numSources: data[i].numSources, 
-				multParcels: data[i].multParcels,
-				reportedSources: data[i].reportedSources	
+				owner: infoOrderData[i].owner, 
+				address: infoOrderData[i].address, 
+				answers: infoOrderData[i].answers,
+				numSources: infoOrderData[i].numSources, 
+				multParcels: infoOrderData[i].multParcels,
+				reportedSources: infoOrderData[i].reportedSources	
 			};
 			console.log(savedData);
 			res.status(200).json(savedData).end(); //if the password matches, send back some data
@@ -37,14 +39,12 @@ app.post('/logIn', function(req, res){
 	}	
 });
 
-app.put('/submit', function(req, res){
+app.put('/infoOrder/submit', function(req, res){
 	//client is sending idCode and answers
 	var idCode = req.body.idCode;
 	var answers = req.body.answers;
-
 	//check how many sources have been reported
 	var reportedSources = 0;
-	
 	for (var j=0; j<5; j++){
 		if(answers[j][4].length>1){ //this is the array of answers to the question 'what is the water source'
 			var reportedSources = reportedSources + 1; 
@@ -54,23 +54,48 @@ app.put('/submit', function(req, res){
 	console.log('reported sources: '+reportedSources)
 
 	//find number of indicated sources
-	for(var i=0; i<data.length; i++){
-		if(data[i].idCode === idCode){
-			data[i].answers = answers; //set equal to client's state
+	for(var i=0; i<infoOrderData.length; i++){
+		if(infoOrderData[i].idCode === idCode){
+			infoOrderData[i].answers = answers; //set equal to client's state
 			if(answers[0][3]){
-				data[i].numSources = answers[0][3]; //set equal to the reported number of sources
+				infoOrderData[i].numSources = answers[0][3]; //set equal to the reported number of sources
 			}
 			else{
-				data[i].numSources = 0; //if this answer space is falsey, there must be 0 sources
+				infoOrderData[i].numSources = 0; //if this answer space is falsey, there must be 0 sources
 			}
-			var numSources = data[i].numSources;
-			data[i].reportedSources = reportedSources; //set equal to newly calculated number of reported sources
+			var numSources = infoOrderData[i].numSources;
+			infoOrderData[i].reportedSources = reportedSources; //set equal to newly calculated number of reported sources
 		}
 	}
 	console.log('numSources: '+numSources);
-
-	
-	res.status(200).json({numSources: numSources, reportedSources: reportedSources});
+	var doc = createPDF(infoOrderData);
+	//send back doc
+	res.status(200).json({numSources: numSources, reportedSources: reportedSources}); 
 });
+
+app.put('/waterRights/submit', function(req, res){
+	var answers = req.body.answers;
+	//push answers to waterRightData object
+	var doc = createPDF(waterRightData);
+	//send back doc
+	res.status(200); 
+});
+
+//create pdf
+var createPDF = function(dataObject){
+	var doc = new PDFDocument;
+	doc.pipe(fs.createWriteStream('record.js')); //alternatively could doc.pipe res
+	doc.font('Helvetica');
+	
+	var page = ; //create a page with 1 source's questions and corresponding answers
+	
+	doc.text(page); 
+	//for each reportedSource after the 1st, add a page
+	doc.addPage();
+	
+	doc.end();
+}
+
+
 var port = (process.env.PORT || 8080);
 app.listen(port);
