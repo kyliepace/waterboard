92890c38e751e4303a215137820e77d38eb5d8a5
Kylie <kyliepace@gmail.com>
2016-08-10 09:57:09 -0700

    Update waterRights.jsx
    set up waterRights form container. Only renders either question or confirmation

--- public/components/waterRights.jsx
+++ public/components/waterRights.jsx
@@ -3,25 +3,71 @@ var Row = require('react-bootstrap').Row;
 var Col = require('react-bootstrap').Col;
 var Question = require('./question.jsx');
 var Button = require('react-bootstrap').Button;
+var Confirm = require('./confirm.jsx');
 
-var WaterRights = function(props){
-	return(
-	    <section>
-	    	
-    		<Col xs={10} xsOffset={1} md={6} mdOffset={3}>
-    			<form className='question'>
-	    			<Question line={'Application ID'} />
-	    			<Question line={'Password'} />
-	    			<Button type='submit'><span className='glyphicon glyphicon-arrow-right' aria-hidden='true'></span></Button>
-    			</form>
-    		</Col>
-
-    		<Col xs={8} xsOffset={2} md={6} mdOffset={3}>
-	    		<h4>Water Rights Reporting</h4>
-	    	</Col>
-	    </section>
-	)
-};
+var WaterRights = React.createClass({
+	componentWillMount: function(props){
+		var index = this.props.params.counter;
+		console.log(this.props);
+		this.props.actions.onLoad(index); //dispatch the reducer to set up the answer objects
+	},
+	handleClick: function(e){
+		var index = this.props.params.counter; //decide what value the index should be
+		this.props.actions.chooseOption(e, index); ///send the action e - which button has been clicked - and index - where in the question array to look
+	},
+	handleChange: function(e){ //when input field changed
+		var index = this.props.params.counter; //decide what value the index should be
+		this.props.actions.changeInput(e, index); 
+	},
+	onSubmit: function(e){
+		e.preventDefault();
+		var that = this;
+		var index = this.props.params.counter; //decide what value the index should be
+		console.log('next will be '+ this.props.waterRights.questions[that.props.params.counter].next);
+		this.props.history.push('/waterRights/'+that.props.waterRights.questions[that.props.params.counter].next);
+		this.props.actions.submitAnswer(index);
+	},
+	sendData: function(){
+		console.log('sending data');
+		var that = this;
+		this.props.actions.submitSource(that.props.waterRights.answers[0][0][0], that.props.infoOrder.answers);
+		this.props.history.push('/'); //return to main menu 
+	},
+	print: function(){
+		this.props.history.push('/print'); //show Print component
+	},
 	
+	render: function(props){
+		var that = this; 
+		var questions = that.props.waterRights.questions; //this is getting the blank infoOrderState, not the state
+		var index = that.props.params.counter;   //get index value from the url params
+		var singleQuestion = questions[index];
+		var answer = that.props.waterRights.answers[index]; //should be an array
+		
+		if(index>=100 && index<1000){
+			var show = (
+				<Confirm sendData={that.sendData} />
+			);
+		}
+		else{
+			var show = (
+				<Question onSubmit={that.onSubmit} onClick={that.prevQuestion} user={that.props.waterRights}
+    			answer = {answer} question={singleQuestion} handleClick={that.handleClick} handleChange={that.handleChange}
+    			changeSource={that.handleClick}/>
+			);
+		}
+		
+		return(
+		<section>
+	    	    {show}
+	
+	    	    <Col xs={8} xsOffset={2} md={6} mdOffset={3}>
+		    	<h4>Water Rights Reporting</h4>
+		    </Col>
+		 </section>
+		)
+	}
+}); 
+
 
 module.exports = WaterRights;
